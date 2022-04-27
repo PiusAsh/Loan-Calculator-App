2705497fa4d19f006a02ef07b48b9d515d3610a5
PiusAsh <Pius1ash@gmail.com>
2022-04-27 12:16:50 +0100

    Adjusted the Interest rate

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ clearBtn.addEventListener('click', clearInputAndResult);
 function showPaymentInfo() {
     let monthlyPayment = calcMonthlyPayment(loanAmount.value, interestRate.value, noOfMonth.value);
     paymentInfoList[0].innerHTML = `₦${loanAmount.value.toLocaleString()}`;
-    paymentInfoList[1].innerHTML = `₦${interestRate.value}%`;
+    paymentInfoList[1].innerHTML = `${interestRate.value}%`;
     paymentInfoList[2].innerHTML = noOfMonth.value;
     paymentInfoList[3].innerHTML = `₦${parseFloat(monthlyPayment).toLocaleString()}`;
 }
